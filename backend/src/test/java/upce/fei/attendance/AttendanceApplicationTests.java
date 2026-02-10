a083966654cf4c8c6b3bb36f5dd13c2c84b1713c
Ondřej Fialka <fialka.ondra@gmail.com>
2026-02-10 11:46:59 +0000

    init

---
+++ backend/src/test/java/upce/fei/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package upce.fei.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
